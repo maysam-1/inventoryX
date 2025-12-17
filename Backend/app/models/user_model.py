f3b295720eddc384f6f6dee48154bdc5c30be37e
maysam-1 <maysamkh44@gmail.com>
2025-12-17 15:50:02 +0200

    added sign up and login

---
+++ Backend/app/models/user_model.py
@@ -0,0 +1,36 @@
+import uuid
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID, ENUM
+from app.utils.database import Base
+
+
+from enum import Enum
+
+class UserRole(str, Enum):
+    admin = "admin"
+    user = "user"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+
+    username = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+
+    role = Column(
+        ENUM(
+            UserRole,
+            name="roles",
+            create_type=False
+        ),
+        nullable=False,
+        default=UserRole.user
+    )
+    password = Column(String, nullable=False)  
+
